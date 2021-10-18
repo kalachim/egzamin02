e18e0180fa27c46cac8e14cb2a20ef82a38b36dd
kalachim <jmichalak2002@gmail.com>
2021-10-18 14:47:53 +0200

    trzecia czesc zadania

---
+++ liga.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piłka nożna</title>
+    <link rel="stylesheet" hred="styl2.css">
+</head>
+<body>
+    <header id= "baner">
+
+    </header>
+
+    <section id="lewa">
+
+    </section>
+
+    <section id="prawa">
+
+    </section>
+
+    <main id="glowny">
+
+    </main>
+
+    <section id="liga">
+
+    </section>
+</body>
+</html>
